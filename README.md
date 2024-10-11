d7c9c0714b6043ccea1265e8615d7c19a4764f60
CaffeineDuck <hello@samrid.me>
2024-10-12 02:10:22 +0545

    feat: add download & install in setup.sh

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 dotfiles.
+
+* Download & install the dotfiles
+```
+curl -sSL https://github.com/CaffeineDuck/dotfiles/blob/master/setup.sh) | bash -s -- d
+```
+
+* CLI Reference
+```
+Usage: ./setup.sh [OPTIONS]
+Manage dotfiles
+
+Options:
+  -h, --help          Display this help message and exit
+  -u, --update-brew   Update/install Homebrew packages (macOS only)
+  -d, --download      Download dotfiles from github and install
+
+Without any options, the script will only create symlinks for dotfiles.
+```
--- setup.sh
+++ setup.sh
@@ -1,6 +1,57 @@
 #!/bin/bash
 
 DOTFILES_DIR="$HOME/projects/dotfiles"
+GITHUB_URL="git@github.com:CaffeineDuck/dotfiles.git"
+UPDATE_BREW=false
+
+function display_help() {
+  echo "Usage: $0 [OPTIONS]"
+  echo "Manage dotfiles"
+  echo
+  echo "Options:"
+  echo "  -h, --help          Display this help message and exit"
+  echo "  -u, --update-brew   Update/install Homebrew packages (macOS only)"
+  echo "  -d, --download      Download dotfiles from github and install"
+  echo
+  echo "Without any options, the script will only create symlinks for dotfiles."
+}
+
+function download_and_run() {
+  if [ -d "$DOTFILES_DIR" ]; then
+    echo "Dotfiles directory already exists. Exiting..."
+    exit 1
+  fi
+
+  mkdir -p "$DOTFILES_DIR"
+  git clone "$GITHUB_URL" "$DOTFILES_DIR"
+  echo "Dotfiles downloaded to $DOTFILES_DIR"
+}
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+  -d | --download)
+    download_and_run
+    ;;
+  -h | --help)
+    display_help
+    exit 0
+    ;;
+  -u | --update-brew)
+    if [[ "$OSTYPE" != darwin* ]]; then
+      echo "Homebrew is only available on macOS. Exiting..."
+      exit 1
+    fi
+    UPDATE_BREW=true
+    ;;
+  *)
+    echo "Unknown parameter passed: $1"
+    display_help
+    exit 1
+    ;;
+  esac
+  shift
+done
 
 function create_symlinks() {
   local items=("$@")
@@ -27,11 +78,17 @@ common_items=(
   "alacritty:$XDG_CONFIG_HOME/alacritty"
 )
 
-# Create symlinks for dotfiles 
+# Create symlinks for dotfiles
 create_symlinks "${common_items[@]}"
 
-# Download packages using brew if mac
 if [[ "$OSTYPE" == darwin* ]]; then
+  # Set icloud directory
+  export ICLOUD=("/Users/caffeineduck/Library/Mobile Documents/com~apple~CloudDocs/")
+  ln -sf "$ICLOUD" ~/icloud
+fi
+
+# Download packages using brew if mac and flag is set
+if [[ "$OSTYPE" == darwin* ]] && [ "$UPDATE_BREW" = true ]; then
   macos_packages=(
     "tmux"
     "neovim"
@@ -39,15 +96,7 @@ if [[ "$OSTYPE" == darwin* ]]; then
     "ripgrep"
     "gh"
   )
-
   for package in "${macos_packages[@]}"; do
     brew install "$package"
   done
-
-  # Set icloud directory
-  export ICLOUD=("/Users/caffeineduck/Library/Mobile Documents/com~apple~CloudDocs/")
-  ln -sf "$ICLOUD" ~/icloud
 fi
-
-
-
